4f690099bfbb887b0cd786705ba4eba1fa8e7ba2
antonio rodriguez <antoniorg1985@hotmail.com>
2020-11-12 20:39:57 +0100

    First commit

---
+++ src/es/studium/Recorre/Recorre.java
@@ -0,0 +1,17 @@
+package es.studium.Recorre;
+
+public class Recorre
+{
+
+	public static void main(String[] args)
+	{
+		String cadena = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		int i;
+		for(i = 0; i<cadena.length(); i++)
+		{
+			System.out.println(cadena.charAt(i));
+		}
+
+	}
+
+}
